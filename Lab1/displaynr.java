4b706092737b02fb15303f773737899b82ecd457
Prishu-04 <pratyakshsrivastava@23ksgmail.com>
2025-12-11 17:10:05 +0530

    Added

--- Lab1/displaynr.java
+++ Lab1/displaynr.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 class displaynr{
      public static void main(String[] args){
-          System.out.println("Name - Pratyaksh");
-          System.out.println("Rollno - 24155040");
-          System.out.println("Section - CSE-48");
-          System.out.println("Branch - CSE (AI & ML)");
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter your Name:");
+         String name=sc.nextLine();
+         System.out.println("Enter your Rollno:");
+         int rollno=sc.nextInt();
+         System.out.println("Enter your Section:");
+         String section=sc.nextLine();
+         System.out.println("Enter your Branch:");
+         String branch=sc.nextLine();
+         System.out.println("Name - "+name);
+          System.out.println("Rollno - "+rollno);
+          System.out.println("Section - "+section);
+          System.out.println("Branch - "+branch);
+          sc.close();
      }
 }
